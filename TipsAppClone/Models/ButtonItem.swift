68735fbf40868ca47e6645a0d268032c3690356a
Jung-creator <junchoia25@gmail.com>
2025-05-03 02:00:54 +0900

    feat: add model

---
+++ TipsAppClone/Models/ButtonItem.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonItem.swift
+//  TipsAppClone
+//
+//  Created by Choi Jung In on 5/3/25.
+//
+
+import Foundation
---
+++ TipsAppClone/ViewModels/SubViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SubViewModel.swift
+//  TipsAppClone
+//
+//  Created by Choi Jung In on 5/3/25.
+//
+
+import Foundation
