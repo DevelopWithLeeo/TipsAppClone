bbdad14765da47db3788b965bd517d75c4f9eb51
maengjj <mjiju06@gmail.com>
2025-05-04 18:53:22 +0900

    feat: MainView 작성

--- TipsAppClone/Views/MainView.swift
+++ TipsAppClone/Views/MainView.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct MainView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var path = NavigationPath() {
+        didSet {
+            print(path)
         }
-        .padding()
     }
+    @State private var searchText: String = ""
+    //이거 이해가 잘 안됨,,,
+    let groupedItems = Dictionary(grouping: MainItem.defaultItem, by: { $0.section })
+    let sortedSections = ["시작하기", "더 찾아보기", "사용자의 기기", "사용 설명서"]
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            VStack {
+                List {
+                    ForEach(sortedSections, id: \.self) { section in
+                        Section(header: Text(section)
+                            .font(.title3)
+                            .foregroundColor(.black)
+                            .fontWeight(.semibold)
+                            .padding(.bottom, 5)
+                        ) {
+                            ForEach(groupedItems[section] ?? []) { item in
+                                if item.isWebView {
+                                    Label(item.title, systemImage: item.icon)
+                                } else {
+                                    NavigationLink(destination: SubView()) {
+                                        Label(item.title, systemImage: item.icon)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("팁")
+            
+        }
+        .searchable(
+            text: $searchText,
+            placement: .navigationBarDrawer(displayMode: .always)
+        )
+    }
+}
+
+#Preview {
+    MainView()
 }
