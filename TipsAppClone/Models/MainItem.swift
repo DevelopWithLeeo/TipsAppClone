f2fd8ffb61ef62ffd77b726b516d5265efa0fc3c
maengjj <mjiju06@gmail.com>
2025-05-05 01:03:21 +0900

    fix: 컬러 gradient으로 수정

--- TipsAppClone/Models/MainItem.swift
+++ TipsAppClone/Models/MainItem.swift
@@ -12,7 +12,7 @@ struct MainItem: Identifiable {
     var title: String
     var subtitle: String
     var icon: String
-    var color: Color
+    var color: LinearGradient
     var detailItemList: [DetailItem]
     var isCountable: Bool
     var isWebView: Bool
@@ -20,15 +20,25 @@ struct MainItem: Identifiable {
 }
 
 extension MainItem {
+    
     static let defaultItem: [MainItem] = [
-        MainItem(id: UUID(), title: "설정 체크리스트", subtitle: "iPhone을 설정하려면 이것부터 시작해보세요.", icon: "checkmark.rectangle.stack", color: .green, detailItemList: [], isCountable: true, isWebView: false, section: "시작하기"),
-        MainItem(id: UUID(), title: "iPhone 탐색하기", subtitle: "", icon: "apps.iphone", color: .blue, detailItemList: [], isCountable: false, isWebView: false, section: "시작하기"),
-        MainItem(id: UUID(), title: "주요 제스처 연습하기", subtitle: "", icon: "hand.draw", color: .purple, detailItemList: [], isCountable: false, isWebView: false, section: "시작하기"),
-        MainItem(id: UUID(), title: "Apple Intellignece", subtitle: "", icon: "sparkles", color: .blue, detailItemList: [], isCountable: false, isWebView: false, section: "새로운 기능"),
-        MainItem(id: UUID(), title: "사진 및 카메라", subtitle: "", icon: "camera", color: .orange, detailItemList: [], isCountable: false, isWebView: false, section: "더 찾아보기"),
-        MainItem(id: UUID(), title: "iPhone 개인 맞춤화하기", subtitle: "", icon: "heart", color: .orange, detailItemList: [], isCountable: false, isWebView: false, section: "더 찾아보기"),
-        MainItem(id: UUID(), title: "Airpods", subtitle: "", icon: "airpods.pro", color: .orange, detailItemList: [], isCountable: false, isWebView: false, section: "사용자의 기기"),
-        MainItem(id: UUID(), title: "iPhone", subtitle: "", icon: "iphone.gen3", color: .orange, detailItemList: [], isCountable: false, isWebView: true, section: "사용 설명서"),
-        MainItem(id: UUID(), title: "Airpods", subtitle: "", icon: "airpods.pro", color: .orange, detailItemList: [], isCountable: false, isWebView: true, section: "사용 설명서")
+        MainItem(id: UUID(), title: "설정 체크리스트", subtitle: "iPhone을 설정하려면 이것부터 시작해보세요.", icon: "checkmark.rectangle.stack",
+                 color: AppGradients.greenToYellow, detailItemList: [], isCountable: true, isWebView: false, section: "시작하기"),
+        MainItem(id: UUID(), title: "iPhone 탐색하기", subtitle: "", icon: "apps.iphone",
+                 color: AppGradients.blueToGreen, detailItemList: [], isCountable: false, isWebView: false, section: "시작하기"),
+        MainItem(id: UUID(), title: "주요 제스처 연습하기", subtitle: "", icon: "hand.draw",
+                 color: AppGradients.blueToCyan, detailItemList: [], isCountable: false, isWebView: false, section: "시작하기"),
+        MainItem(id: UUID(), title: "Apple Intellignece", subtitle: "", icon: "apple.intelligence",
+                 color: AppGradients.redToOrange, detailItemList: [], isCountable: false, isWebView: false, section: "새로운 기능"),
+        MainItem(id: UUID(), title: "사진 및 카메라", subtitle: "", icon: "camera",
+                 color: AppGradients.orangeToYellow, detailItemList: [], isCountable: false, isWebView: false, section: "더 찾아보기"),
+        MainItem(id: UUID(), title: "iPhone 개인 맞춤화하기", subtitle: "", icon: "heart",
+                 color: AppGradients.purpleToPink, detailItemList: [], isCountable: false, isWebView: false, section: "더 찾아보기"),
+        MainItem(id: UUID(), title: "Airpods", subtitle: "", icon: "airpods.pro",
+                 color: AppGradients.cyanToTeal, detailItemList: [], isCountable: false, isWebView: false, section: "사용자의 기기"),
+        MainItem(id: UUID(), title: "iPhone", subtitle: "", icon: "iphone.gen3",
+                 color: AppGradients.indigoToBlue, detailItemList: [], isCountable: false, isWebView: true, section: "사용 설명서"),
+        MainItem(id: UUID(), title: "Airpods", subtitle: "", icon: "airpods.pro",
+                 color: AppGradients.indigoToBlue, detailItemList: [], isCountable: false, isWebView: true, section: "사용 설명서")
     ]
 }
