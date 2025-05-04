f29660d8d90298a91549adf7d9876fc9896709c3
maengjj <mjiju06@gmail.com>
2025-05-04 18:27:05 +0900

    fix: MainItem 모델 추가(섹션)

--- TipsAppClone/Models/MainItem.swift
+++ TipsAppClone/Models/MainItem.swift
@@ -5,7 +5,6 @@
 //  Created by Choi Jung In on 5/2/25.
 //
 
-import Foundation
 import SwiftUI
 
 struct MainItem: Identifiable {
@@ -17,4 +16,19 @@ struct MainItem: Identifiable {
     var detailItemList: [DetailItem]
     var isCountable: Bool
     var isWebView: Bool
+    var section: String
+}
+
+extension MainItem {
+    static let defaultItem: [MainItem] = [
+        MainItem(id: UUID(), title: "설정 체크리스트", subtitle: "iPhone을 설정하려면 이것부터 시작해보세요.", icon: "checkmark.square", color: .green, detailItemList: [], isCountable: true, isWebView: false, section: "시작하기"),
+        MainItem(id: UUID(), title: "iPhone 탐색하기", subtitle: "", icon: "iphone", color: .blue, detailItemList: [], isCountable: false, isWebView: false, section: "시작하기"),
+        MainItem(id: UUID(), title: "주요 제스처 연습하기", subtitle: "", icon: "hand.tap", color: .purple, detailItemList: [], isCountable: false, isWebView: false, section: "시작하기"),
+        MainItem(id: UUID(), title: "Apple Intellignece", subtitle: "", icon: "sparkles", color: .blue, detailItemList: [], isCountable: false, isWebView: false, section: "새로운 기능"),
+        MainItem(id: UUID(), title: "사진 및 카메라", subtitle: "", icon: "atom", color: .orange, detailItemList: [], isCountable: false, isWebView: false, section: "더 찾아보기"),
+        MainItem(id: UUID(), title: "iPhone 개인 맞춤화하기", subtitle: "", icon: "atom", color: .orange, detailItemList: [], isCountable: false, isWebView: false, section: "더 찾아보기"),
+        MainItem(id: UUID(), title: "Airpods", subtitle: "", icon: "atom", color: .orange, detailItemList: [], isCountable: false, isWebView: false, section: "사용자의 기기"),
+        MainItem(id: UUID(), title: "iPhone", subtitle: "", icon: "atom", color: .orange, detailItemList: [], isCountable: false, isWebView: true, section: "사용 설명서"),
+        MainItem(id: UUID(), title: "Airpods", subtitle: "", icon: "atom", color: .orange, detailItemList: [], isCountable: false, isWebView: true, section: "사용 설명서")
+    ]
 }
