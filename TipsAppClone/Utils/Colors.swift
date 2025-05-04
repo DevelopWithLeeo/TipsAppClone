9499298eea684beb51a9c174e5ca8ef2767abddf
maengjj <mjiju06@gmail.com>
2025-05-05 01:02:35 +0900

    feat: Colors파일에 GradeintColor 추가

--- TipsAppClone/Utils/Colors.swift
+++ TipsAppClone/Utils/Colors.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct AppGradients {
+    static let greenToYellow = LinearGradient(colors: [.green, .yellow], startPoint: .bottomLeading, endPoint: .topTrailing)
+    static let blueToGreen = LinearGradient(colors: [.blue, .green], startPoint: .bottomLeading, endPoint: .topTrailing)
+    static let blueToCyan = LinearGradient(colors: [.blue, .cyan], startPoint: .bottomLeading, endPoint: .topTrailing)
+    static let redToOrange = LinearGradient(colors: [.red, .orange], startPoint: .bottomLeading, endPoint: .topTrailing)
+    static let orangeToYellow = LinearGradient(colors: [.orange, .yellow], startPoint: .bottomLeading, endPoint: .topTrailing)
+    static let purpleToPink = LinearGradient(colors: [.purple, .pink], startPoint: .bottomLeading, endPoint: .topTrailing)
+    static let cyanToTeal = LinearGradient(colors: [.cyan.opacity(0.8), .teal.opacity(0.6)], startPoint: .bottomLeading, endPoint: .topTrailing)
+    static let indigoToBlue = LinearGradient(colors: [.indigo, .blue], startPoint: .bottomLeading, endPoint: .topTrailing)
+
+}
